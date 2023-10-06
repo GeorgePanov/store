6674b1bb5d073fd8d6e255784bad00ece1965503
GeorgePanov <georgepanov19@gmail.com>
2023-10-07 00:45:05 +0300

    Categories

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Poster from '../Poster/Poster';
 import Products from '../Products/Products'
 import { useSelector } from 'react-redux';
+import Categories from '../Categories/Categories';
 const Home = () => {
-    const { list } = useSelector(({ products }) => products)
+    const { products, categories } = useSelector((state) => state)
+
     return (
         <>
             <Poster />
-            <Products title="Trending" products={list} amount={5} />
+            <Products title="Trending" products={products.list} amount={5} />
+            <Categories title="Worth seeing" products={categories.list} amount={5} />
         </>
     );
 };
