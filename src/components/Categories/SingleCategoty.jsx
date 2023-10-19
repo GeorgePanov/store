9bef09e54cbfdf7f2b441078e6a63f7b88551b2b
GeorgePanov <georgepanov19@gmail.com>
2023-10-19 19:43:34 +0300

    Categories pages

---
+++ src/components/Categories/SingleCategoty.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Poster from './../Poster/Poster';
+import Category from './Category';
+
+const SingleCategoty = () => {
+    return (
+        <>
+            <Poster />
+            <Category />
+        </>
+    );
+};
+
+export default SingleCategoty;
--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -6,12 +6,14 @@ import { ROUTES } from "../../utils/routes";
 import Home from "../Home/Home";
 import SingleProduct from "../Products/SingleProduct";
 import Profile from "../Profile/Profile";
+import SingleCategoty from "../Categories/SingleCategoty";
 
 const AppRoutes = () => (
     <Routes>
         <Route index element={<Home />} />
         <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
         <Route path={ROUTES.PROFILE} element={<Profile />} />
+        <Route path={ROUTES.CATEGORY} element={<SingleCategoty />} />
     </Routes>
 );
 
